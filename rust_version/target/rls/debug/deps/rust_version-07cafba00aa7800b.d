a262199fbeef964bd3ebcafc0df8159d0a1543cf
Alan M <alan@MacBook-Pro-de-MacBook.local>
2019-06-20 23:30:12 -0500

    rust stuff

---
+++ rust_version/target/rls/debug/deps/rust_version-07cafba00aa7800b.d
@@ -0,0 +1,5 @@
+/Users/alan/Documents/C-Interpreter/rust_version/target/rls/debug/deps/rust_version-07cafba00aa7800b.rmeta: src/main.rs
+
+/Users/alan/Documents/C-Interpreter/rust_version/target/rls/debug/deps/rust_version-07cafba00aa7800b.d: src/main.rs
+
+src/main.rs:
---
+++ rust_version/target/rls/debug/deps/rust_version-08d4d607ae24c718.d
@@ -0,0 +1,5 @@
+/Users/alan/Documents/C-Interpreter/rust_version/target/rls/debug/deps/rust_version-08d4d607ae24c718.rmeta: src/main.rs
+
+/Users/alan/Documents/C-Interpreter/rust_version/target/rls/debug/deps/rust_version-08d4d607ae24c718.d: src/main.rs
+
+src/main.rs:
