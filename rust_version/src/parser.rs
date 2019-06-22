20080560cd14e408612fe91bc2096baa69a7ba5a
Alan M <alan@MacBook-Pro-de-MacBook.local>
2019-06-22 16:44:06 -0500

    try to fix ignore

--- rust_version/src/parser.rs
+++ rust_version/src/parser.rs
@@ -3,13 +3,15 @@ use std::io::Write;
 use std::error;
 
 
-pub fn parse_line<'a>() -> Result<String, Box<error::Error> {
+pub fn parse_line<'a>() -> Result<String, Box<error::Error>> {
     let mut buffer = String::new();
     print!("calc> ");
     io::stdout().flush().unwrap(); // Makes sense, but ugh.
     io::stdin().read_line(&mut buffer)?;
 
     let token_vector : Vec<&'a str> = parse_tokens(buffer)?;
+
+    Ok("All Correct!".into())
 }
 
 fn parse_tokens<'a>(buffer : String)-> Result<Vec<&'a str>,String>{
